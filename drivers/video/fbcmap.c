59e9aa7a41affbf30589a1c030a1c84fb29a2772
Kees Cook <keescook@chromium.org>
2017-01-24 15:18:24 -0800

    fbdev: color map copying bounds checking
    commit 2dc705a9930b4806250fbf5a76e55266e59389f2 upstream.
    
    Copying color maps to userspace doesn't check the value of to->start,
    which will cause kernel heap buffer OOB read due to signedness wraps.
    
    CVE-2016-8405
    
    Link: http://lkml.kernel.org/r/20170105224249.GA50925@beast
    Fixes: 1da177e4c3f4 ("Linux-2.6.12-rc2")
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Reported-by: Peter Pi (@heisecode) of Trend Micro
    Cc: Min Chong <mchong@google.com>
    Cc: Dan Carpenter <dan.carpenter@oracle.com>
    Cc: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Cc: Bartlomiej Zolnierkiewicz <b.zolnierkie@samsung.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- drivers/video/fbcmap.c
+++ drivers/video/fbcmap.c
@@ -166,9 +166,6 @@ int fb_copy_cmap(const struct fb_cmap *from, struct fb_cmap *to)
 	unsigned int tooff = 0, fromoff = 0;
 	size_t size;
 
-	if (!to || !from)
-		return -EINVAL;
-
 	if (to->start > from->start)
 		fromoff = to->start - from->start;
 	else
@@ -197,14 +194,12 @@ int fb_cmap_to_user(const struct fb_cmap *from, struct fb_cmap_user *to)
 	unsigned int tooff = 0, fromoff = 0;
 	size_t size;
 
-	if (!to || !from || (int)(to->start) < 0)
-		return -EINVAL;
-
 	if (to->start > from->start)
 		fromoff = to->start - from->start;
 	else
 		tooff = from->start - to->start;
-	if ((to->len <= tooff) || (from->len <= fromoff))
+
+	if (fromoff >= from->len || tooff >= to->len)
 		return -EINVAL;
 
 	size = min_t(size_t, to->len - tooff, from->len - fromoff);
